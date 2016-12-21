99822beb90884b561d88fa1d02980869a8a6cad9
Ben Webber <benjamin.webber@gmail.com>
2016-12-21 15:47:59 +0800

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='curlrc',
-    version='0.2.0',
+    version='0.2.1',
     url='https://github.com/benwebber/curlrc/',
 
     description="Treat curl configuration files as curlrc subcommands.",
